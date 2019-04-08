ef2a95493786b1402fa8382e52eaf3f1e7874c6d
Quentin Weber <webquentin@web.de>
2019-04-08 14:50:16 +0200

    added cityObjects class

---
+++ CityObjects.cs
@@ -0,0 +1,19 @@
+﻿
+using UnityEngine;
+
+namespace Assets
+{
+    public class CityObjects
+    {
+        public GameObject[] stages;
+        public GameObject[] groundFloors;
+        public GameObject[] rooftops;
+
+        public CityObjects(GameObject[] stages, GameObject[] grounFloors, GameObject[] rooftops)
+        {
+            this.stages = stages;
+            this.groundFloors = groundFloors;
+            this.rooftops = rooftops;
+        }
+    }
+}
